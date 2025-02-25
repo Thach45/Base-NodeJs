05aa7c8b2ed358e34dc67f21e92da9f63668e14c
Thach45 <nguyenhoangthach2005@gmail.com>
2025-02-25 14:59:46 +0700

    chore: update gitignore rules

--- dist/helper/genToken.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.genToken = void 0;
-const genToken = (length) => {
-    return Math.random().toString(36).substring(2, length + 2);
-};
-exports.genToken = genToken;
